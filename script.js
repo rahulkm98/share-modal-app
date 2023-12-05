ad59f81f3a057d1afd3a15571ce84102a1b98a58
rahulkm98 <rahulkumar1998rkm@gmail.com>
2023-12-05 16:30:27 +0530

    share model app

---
+++ script.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay");
+
+//modal open function
+const openModal = () => {
+    console.log("Modal is open")
+    modal.classList.add("active");
+    overlay.classList.add("overlayactive");
+};
+//modal close function
+const closeModal = () => {
+    modal.classList.remove("active");
+    overlay.classList.remove("overlayactive");
+};
